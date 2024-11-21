43fe2a1b824410b4694e63430651a7f184a8657f
Manu <emmanuel.donahue@hotmail.fr>
2024-11-21 14:42:36 +0100

    suite

--- src/componants/ThirdPartHome.tsx
+++ src/componants/ThirdPartHome.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import '../styles/thirdparthome.css';
 
 const ThirdPartHome: React.FC = () => {
 
     return (
-        <h1>componant troisième partie</h1>
+        <section className="third-section">
+            <div className="contain-third-section">
+                <h1>parcours utilisateur</h1>
+                <div className="test">
+                    <span>bonjour</span>
+                </div>
+                <div className="test1">
+                </div>
+                <div className="test">
+                    <span>test</span>
+                </div>
+                <div className="test2"></div>
+                <div className="test"></div>
+            </div>
+        </section>
     )
 }
 
